89b8ca92443fbc90ec82064d845e68763cba4e7c
Jack Xie <jxie@riotgames.com>
2021-04-04 19:04:01 -0700

    add Public view & make it default

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,17 +2,24 @@ import 'package:get/get.dart';
 
 import 'package:flutter_getx_template/app/modules/home/bindings/home_binding.dart';
 import 'package:flutter_getx_template/app/modules/home/views/home_view.dart';
+import 'package:flutter_getx_template/app/modules/public/bindings/public_binding.dart';
+import 'package:flutter_getx_template/app/modules/public/views/public_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.PUBLIC;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.PUBLIC,
+      page: () => PublicView(),
+      binding: PublicBinding(),
     ),
   ];
 }
